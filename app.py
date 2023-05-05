36f257dd4aaf8fc9e59c1557057ea5ea2c4a5d4e
nopeter913 <65943245+nopeter913@users.noreply.github.com>
2023-05-05 18:40:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,105 @@ def handle_message(event):
 import os if __name__ == "__main__":    
 　　port = int(os.environ.get('PORT', 5000))     
 　　app.run(host='0.0.0.0', port=port)
+import warnings
+warnings.filterwarnings("ignore")
 
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+import matplotlib.pyplot as plt
+import pyfolio as pf
+
+from models import BollingerModel, KdModel, RsiModel
+
+MAX_TRY = 3
+
+def user_input(max_try):
+    for _ in range(max_try):
+
+        stock = input("Please enter the stock name: ")
+
+        try:
+            days = int(input("Please enter how many days you want to test from now: "))
+            end_day = dt.datetime.today()
+        except:
+            raise ValueError("The days you input is not valid !")
+            continue
+
+        try:
+            capital = int(input("Please enter the capital you want to invest: "))
+        except:
+            raise ValueError("The capital you input is not valid !")
+            continue
+
+        try:
+            strategy = int(input("Please choose the strategy you want to use(enter one of the numbers below):\n1.Boll 2.KD 3.RSI\n"))
+        except:
+            raise ValueError("The number you input is not valid!")
+            continue
+        if strategy < 1 or strategy > 3:
+            print('The number must be in the range 1~3')
+            continue
+
+        break
+        
+    return stock, days, end_day, capital, strategy
+            
+
+def write_xlsx(self, file_name):
+    with pd.ExcelWriter(file_name) as writer:
+        self.data.to_excel(writer)
+
+
+if __name__ == "__main__":
+    stock, days, end_day, capital, strategy = user_input(MAX_TRY)
+    # users choose Bollinger
+    if strategy == 1:
+        boll = BollingerModel(symbol=stock, end_day=end_day, days=days, capital=capital)
+        try:
+            start = int(input("Please input the minimum MA you want to use: "))
+            end = int(input("Please input the maximum MA you want to use: "))
+        except:
+            raise ValueError("The MA you input is not valid!")
+        if start > end:
+            print('The number must be in the range 1~3')
+            exit()
+        best_window, best_dr, best_sr = boll.optimizer(start, end)
+        print(best_window, best_dr, best_sr)
+        boll.plot_equity_curve(best_window)
+    
+    # users choose KD
+    elif strategy == 2:
+        try:
+            start = int(input("Please input the minimum number of days in the past you want to use: "))
+            end = int(input("Please input the maximum number of days in the past you want to use: "))
+        except:
+            raise ValueError("The the number of days you input is not valid!")
+        if start > end:
+            print('The number must be in the range 1~3')
+            exit()
+        k_value = 50
+        kd = KdModel(symbol=stock, end_day=end_day, days=days, capital=capital, k=k_value)
+        best_window, best_dr, best_sr = kd.optimizer(start, end)
+        print(best_window, best_dr, best_sr)
+        kd.plot_equity_curve(best_window)
+    
+    # users choose RSI
+    elif strategy == 3:
+        try:
+            start_short = int(input("Please input the minimum slow line you want to use: "))
+            end_short = int(input("Please input the maximum slow line you want to use: "))
+            start_long = int(input("Please input the minimum long line you want to use: "))
+            end_long = int(input("Please input the maximum long line you want to use: "))
+        except:
+            raise ValueError("The the number of days you input is not valid!")
+        if (start_short > end_short) or (start_long > end_long) or \
+            (end_short > start_long):
+                print('The number of days is out of the range')
+                exit()
+        rsi = RsiModel(symbol=stock, end_day=end_day, days=days, capital=capital)
+        max_short, max_long, max_dr, max_sr = \
+            rsi.optimizer(start_short, end_short, start_long, end_long)
+        print(max_short, max_long, max_dr, max_sr)
+        rsi.plot_equity_curve(max_short, max_long)
