77ecf002fd36b7003abd80f3cd198b42f07c401b
nopeter913 <65943245+nopeter913@users.noreply.github.com>
2023-05-08 23:34:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,34 @@
-heroku create myapp --buildpack heroku/python
-from flask import Flask, request, abort  
-from linebot import (
-　　LineBotApi, WebhookHandler
- ) 
-from linebot.exceptions import (
-　　InvalidSignatureError
- ) 
-from linebot.models import *
+from flask_ngrok import run_with_ngrok
+from flask import Flask, request
 
-app = Flask(__name__)  
-# 必須放上自己的Channel Access Token 
-line_bot_api = LineBotApi('+NVSn1t0cmsD/NEdD7VxRf20f6ycv0O9ypddg1j8k4PB+h5o5DYQu70NDZKNWuKyiyEdARphwIRNt+9dvYyqA5NX35ilcxyivu9x/jCONecgMWqlJLYiLrmPHhQNucQarqbz8mMOIaB9tsFG7Hs9BgdB04t89/1O/w1cDnyilFU=')  
-# 必須放上自己的Channel Secret
- handler = WebhookHandler('d10ad5359e10414a8bd074926ae0e643')
-line_bot_api.push_message('Ue4d3f17768ea75f3bfa388142bf4b3b5', TextSendMessage(text='你可以開始了'))
+# 載入 LINE Message API 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-# 監聽所有來自 /callback 的 Post Request 
-@app.route("/callback", methods=['POST']) 
-def callback():     
-　　# get X-Line-Signature header value     
-　　signature = request.headers['X-Line-Signature']
-　　# get request body as text     
-　　body = request.get_data(as_text=True)     
-　　app.logger.info("Request body: " + body)      
-　　# handle webhook body     
-　　try:         
-　　　　handler.handle(body, signature)     
-　　except InvalidSignatureError:         
-　　　　abort(400)      
-　　return 'OK'
+# 載入 json 標準函式庫，處理回傳的資料格式
+import json
 
-#訊息傳遞區塊 
-##### 基本上程式編輯都在這個function ##### 
-@handler.add(MessageEvent, message=TextMessage) 
-def handle_message(event):     
-　　message = event.message.text     
-　　line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
-  
-  #主程式 
-import os if __name__ == "__main__":    
-　　port = int(os.environ.get('PORT', 5000))     
-　　app.run(host='0.0.0.0', port=port)
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    try:
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = '你的 LINE Channel access token'
+        secret = '你的 LINE Channel secret'
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        line_bot_api.reply_message(tk,TextSendMessage(msg))  # 回傳訊息
+        print(msg, tk)                                       # 印出內容
+    except:
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                 # 驗證 Webhook 使用，不能省略
+if __name__ == "__main__":
+  run_with_ngrok(app)           # 串連 ngrok 服務
+  app.run()
